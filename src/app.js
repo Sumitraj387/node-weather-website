0b138a48ded5ef57732aaf453165c95b1d216c7a
Sumit Raj <1801cs53@iitp.ac.in>
2019-12-04 19:49:47 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,96 @@
+const express=require('express')
+const path=require('path')
+const hbs=require('hbs')
+const request=require('request')
+const geocode=require('./utils/geocode')
+const forecast=require('./utils/forecasts')
+const app=express()
+//define paths for express configuration
+const publicDirectoryPath=path.join(__dirname,'../public')
+const viewsPath=path.join(__dirname,'../templates/views')
+const partialsPath=path.join(__dirname,'../templates/partials')
+//setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+app.get('',(req,res)=>
+{
+    res.render('index',{
+        title:'Weather',
+        name:'Sumit Raj'
+    })
+})
+app.get('/about',(req,res)=>
+{
+    res.render('about',{
+        title:'about me',
+        name:'Sumit raj'
+    })
+})
+app.get('/help',(req, res)=>
+{
+    res.render('help',{
+        title:'help',
+        name:'Sumit Raj'
+    })
+})
+app.get('/weather',(req,res)=>
+{
+    if(!req.query.address)
+    {
+        return res.send({
+            error:'You must provide an address'
+        })
+    }
+    geocode(req.query.address,(error,data={})=>{
+        if(error)
+        {
+            return res.send({error})
+        }
+        forecast(data.latitude,data.longitude,(error,forecastData)=>{
+            if(error)
+            {
+                return res.send({error})
+            }
+            res.send({
+                forecast:forecastData,
+                location:req.query.address
+            })
+        })
+    })
+})
+app.get('/products',(req,res)=>
+{
+    if(!req.query.search)
+    {
+           return res.send({
+                error:'You must provide a search term'
+            })
+    }
+    console.log(req.query.search)
+    res.send({
+        products:[]
+    })
+})
+app.get('/help/*',(req,res)=>
+{
+    res.render('4O4',{
+        title:'4O4',
+        name:'Sumit Raj',
+        errorMessage:'Help article not found'
+    })
+})
+app.get('*',(req,res)=>
+{
+    res.render('4O4',{
+        title:'4O4',
+        name:'Sumit Raj',
+        errorMessage:'Page not found'
+    })
+})
+app.listen(3000,()=>
+{
+   console.log('server is up on the port 3000')
+})
